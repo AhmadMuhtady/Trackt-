85a802d3c3bee72ae25bb218a703d42cbfb340d6
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2026-04-18 03:52:09 +0300

    added WorkoutStore class

---
+++ src/services/WorkoutStore.js
@@ -0,0 +1,24 @@
+export class WorkoutStore {
+	constructor() {
+		this.workouts = this.getWorkouts();
+	}
+	saveWorkouts() {
+		localStorage.setItem('workouts', JSON.stringify(this.workouts));
+	}
+
+	getWorkouts() {
+		const stored = localStorage.getItem('workouts');
+		return stored ? JSON.parse(stored) : [];
+	}
+
+	reset() {
+		this.workouts = [];
+		this.saveWorkouts();
+	}
+
+	deleteWorkout(workoutId) {
+		this.workouts = this.workouts.filter((workout) => workout.id !== workoutId);
+		this.saveWorkouts();
+		return this.workouts;
+	}
+}
